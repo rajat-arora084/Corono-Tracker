f82845113c6b0c5f8c1648a4b8040067afd87c16
Rajat Arora <arora.rajat084@gmail.com>
2020-04-07 16:10:09 +0530

    Changed Coron cases dict
    Made the code in try-catch block.

--- main.py
+++ main.py
@@ -8,42 +8,45 @@ from log_file import LogFile
 # Utility Function to find the Live cases & provide the infor via desktop notification.
 def main():
 
-    corona_cases_parser = ParseCovidCases()
-    corona_cases = corona_cases_parser.main()
+    try:
+        corona_cases_parser = ParseCovidCases()
+        corona_cases = corona_cases_parser.main() or dict()
 
-    current_cases_parser = ParseCurrentCases()
-    current_cases = current_cases_parser.find_current_cases()
-    print(corona_cases, current_cases)
+        current_cases_parser = ParseCurrentCases()
+        current_cases = current_cases_parser.find_current_cases()
+        print(corona_cases, current_cases)
 
-    log_file = LogFile()
-    log_file.write(corona_cases)
+        log_file = LogFile()
+        log_file.write(corona_cases)
 
-    desktop_notifier = DesktopNotification()
+        desktop_notifier = DesktopNotification()
 
-    data_updated = False
+        data_updated = False
 
-    for key in corona_cases.keys():
-        if corona_cases[key] != current_cases[key]:
-            data_updated = True
-            break
+        for key in corona_cases.keys():
+            if corona_cases[key] != current_cases[key]:
+                data_updated = True
+                break
 
-    if data_updated:
-        if current_cases_parser.update_data(corona_cases):
-            log_file.write('Data Updated in Current Cases txt')
+        if data_updated:
+            if current_cases_parser.update_data(corona_cases):
+                log_file.write('Data Updated in Current Cases txt')
+            else:
+                message = 'Please manually update the corona cases.\nOtherwise you will get repeated updates.'
+                log_file.write(message)
+                print(message)
+            desktop_notifier.show_notification(corona_cases)
         else:
-            message = 'Please manually update the corona cases.\nOtherwise you will get repeated updates.'
-            log_file.write(message)
-            print(message)
-        desktop_notifier.show_notification(corona_cases)
-    else:
-        print('Same data')
+            print('Same data')
+    except Exception as err:
+        print('Some error occured while fetching the data. Pls check your Internet connection. \n{}'.format(err.message))
 
 
 def run():
     while 1:
         main()
-        time.sleep(900)
+        time.sleep(300)
 
 
 if __name__ == '__main__':
-    run()
+    run() #main()
